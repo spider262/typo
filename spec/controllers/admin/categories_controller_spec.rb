73588c6e95abe0ab60f4479609957a2f24e2b93b
EC2 Default User <ec2-user@ip-172-31-38-1.us-east-2.compute.internal>
2019-11-04 21:02:20 +0000

    finish spec

--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -16,6 +16,25 @@ describe Admin::CategoriesController do
     assert_response :redirect, :action => 'index'
   end
 
+  describe "test_create" do
+    before(:each) do 
+      get :new
+    end
+
+    it 'should render template new' do
+      assert_template 'new'
+      assert_tag :tag => "table",
+        :attributes => { :id => "category_container" }
+    end
+
+    it "shoud create new category" do
+      post:new, :category => {:name => "new_cate", :keywords => "keys", :permalink => "GG"}
+      assert_response :redirect, :action=> "index"
+      expect(assigns(:category)).not_to be_nil
+      expect(flash[:notice]).to eq("Category was successfully saved.")
+    end
+  end
+  
   describe "test_edit" do
     before(:each) do
       get :edit, :id => Factory(:category).id
@@ -25,6 +44,13 @@ describe Admin::CategoriesController do
       assert_template 'new'
       assert_tag :tag => "table",
         :attributes => { :id => "category_container" }
+    end
+    
+    it "shoud edit category" do
+      post:edit, :category => {:name => "general", :keywords => "keys", :permalink => "GG"}
+      assert_response :redirect, :action=> "index"
+      expect(assigns(:category)).not_to be_nil
+      expect(flash[:notice]).to eq("Category was successfully saved.")
     end
 
     it 'should have valid category' do
